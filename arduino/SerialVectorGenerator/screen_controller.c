a1fdb1bd22bb7cacb0f9e411b0f58f26c240c915
Charles Franklin <cjfman@gmail.com>
2024-11-09 20:16:00 -0500

    Add line calculator

--- arduino/SerialVectorGenerator/screen_controller.c
+++ arduino/SerialVectorGenerator/screen_controller.c
@@ -16,18 +16,34 @@ static inline int get_abs_pos(int val, int scale, int offset) {
 
 }
 
-static calcPoint(const PointCmd* cmd, const ScreenState* screen, BeamState* beam) {
+static void calcPointHelper(int x, int y, const ScreenState* screen, BeamState* beam) {
 	// Scale and offset position
 	long diff = SCREEN_MAX_VALUE - SCREEN_MIN_VALUE;
-	beam->x = diff * get_abs_pos(cmd->x, screen->x_scale, screen->x_offset) / screen->x_scale - SCREEN_MIN_VALUE;
-	beam->y = diff * get_abs_pos(cmd->y, screen->y_scale, screen->y_offset) / screen->y_scale - SCREEN_MIN_VALUE;
+	beam->x = diff * get_abs_pos(x, screen->x_scale, screen->x_offset) / screen->x_scale - SCREEN_MIN_VALUE;
+	beam->y = diff * get_abs_pos(y, screen->y_scale, screen->y_offset) / screen->y_scale - SCREEN_MIN_VALUE;
 	beam->a = 1;
 }
 
-void nextScreenState(const int elapsed_time, const Command* cmd, const ScreenState* screen, BeamState* beam) {
+static void calcPoint(const PointCmd* cmd, const ScreenState* screen, BeamState* beam) {
+	calcPointHelper(cmd->x, cmd->y, screen, beam);
+}
+
+void calcLine(int elapsed_time_ms, const LineCmd* cmd, const ScreenState* screen, BeamState* beam) {
+	float completed = (float)elapsed_time_ms / cmd->ms;
+	completed = (completed > 1) ? completed : 1;
+	int x = (cmd->x2 - cmd->x1) * completed + cmd->x1;
+	int y = (cmd->y2 - cmd->y1) * completed + cmd->y1;
+	calcPointHelper(x, y, screen, beam);
+}
+
+void nextScreenState(int elapsed_time_ms, const Command* cmd, const ScreenState* screen, BeamState* beam) {
 	switch (cmd->type) {
 	case Cmd_Point:
 		calcPoint(cmd, screen, beam);
+		break;
+	case Cmd_Line:
+		calcLine(elapsed_time_ms, cmd, screen, beam);
+		break;
 	default:
 		beam->x = 0;
 		beam->y = 0;
