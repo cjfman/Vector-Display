08bbc082dcf7d027008e0ff8d4d118ef60dbbda6
Charles Franklin <cjfman@gmail.com>
2024-11-23 20:22:49 -0500

    Add line test

--- arduino/UnitTests/command_parser_tests.cpp
+++ arduino/UnitTests/command_parser_tests.cpp
@@ -55,3 +55,20 @@ TEST_F(CommandParserTest, point) {
     EXPECT_EQ(42, point_cmd->x);
     EXPECT_EQ(-5, point_cmd->y);
 }
+
+TEST_F(CommandParserTest, line) {
+    // Send and parse command
+    const char cmd_str[] = "line -2 0 452 87\r\n";
+    this->build_command(cmd_str, sizeof(cmd_str));
+    CommandUnion cmd;
+    ASSERT_EQ(CMD_OK, cmdParse(&cmd, this->cmd_buf, CMD_BUF_SIZE))
+        << "Base command: " << cmd.base.buf << "; Num args: " << cmd.base.numargs;
+
+    // Check parsed command
+    ASSERT_EQ(Cmd_Line, cmd.base.type);
+    LineCmd* line_cmd = (LineCmd*)&cmd;
+    EXPECT_EQ(-2,  line_cmd->x1);
+    EXPECT_EQ(0,   line_cmd->y1);
+    EXPECT_EQ(452, line_cmd->x2);
+    EXPECT_EQ(87,  line_cmd->y2);
+}
